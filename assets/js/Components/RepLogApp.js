b0f85c409ea5e2efa6389b12f48d795cbd544c50
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-06 10:49:10 +0700

    Integrating FOSJsRoutingBundle

--- assets/js/Components/RepLogApp.js
+++ assets/js/Components/RepLogApp.js
@@ -3,6 +3,9 @@
 const Helper = require('./RepLogHelper');
 const $ = require('jquery');
 const swal = require('sweetalert2');
+const Routing = require('./Routing');
+
+
 
 let HelperInstances = new WeakMap();
 
