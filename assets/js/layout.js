99663ad6ae5c5431d0224a057e562e5322b96e1b
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-06 10:42:34 +0700

    Sass & Sourcemaps

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -6,7 +6,7 @@ global.$ = $; // use for upgrade to webpack
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.min.css');
 require('font-awesome/css/font-awesome.css');
-require('../css/main.css');
+require('../css/main.scss');
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ var Encore = require('@symfony/webpack-encore');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
-// the project directory where all compiled assets will be stored
+
+    // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
 
     // the public path used by the web server to access the previous directory
@@ -21,6 +22,9 @@ Encore
     .addPlugin(new CopyWebpackPlugin([
         { from: './assets/static', to: 'static' }
     ]))
+
+    .enableSassLoader()
+    .enableSourceMaps(!Encore.isProduction())
 
 ;
 
