07d3b7bf3e3c35a7b90c8946912ef66cfa56ffd1
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-06 09:47:11 +0700

    jQuery Plugins / Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,12 @@ Encore
 
     // will create public/build/app.js and public/build/app.css
     .addEntry('rep_log', './public/assets/js/rep_log.js')
+    .addEntry('login', './public/assets/js/login.js')
+    .addEntry('layout', './public/assets/js/layout.js')
 
     .enableBuildNotifications()
+
+    .autoProvidejQuery()
 
 ;
 
